78cbf362e2b694286b0f09b617f423c9ff5ec74a
drvietcanh <drvietcanh@gmail.com>
2026-09-17 22:24:56 +0700

    Ket noi nguon khoa phong tu Google Sheets

--- bvqy13_web_v1/assets/config.js
+++ bvqy13_web_v1/assets/config.js
@@ -4,5 +4,8 @@ window.BV13_CONFIG={
   hotline:"",
   // Khi Google Sheets sẵn sàng, dán URL CSV của từng sheet vào đây.
   // Để trống thì website dùng dữ liệu mẫu tích hợp bên dưới.
-  sheets:{departments:"",articles:"",services:"",guides:""}
+  sheets:{
+    departments:"https://docs.google.com/spreadsheets/d/e/2PACX-1vQgZcyBUCTqKEhkZeDU9OOSx-fI4ZrGWhQ2C7Jox-o99aTt2eiKnrtpx4Jhx9dX6g/pub?gid=1522592803&single=true&output=csv",
+    articles:"",services:"",guides:""
+  }
 };
