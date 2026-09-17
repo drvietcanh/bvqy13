3481974b801bda23d36d28c4c81706a5b0b85aba
drvietcanh <drvietcanh@gmail.com>
2026-09-17 22:31:46 +0700

    Chuyen nguon du lieu sang file Sheet V3 dung

--- bvqy13_web_v1/assets/config.js
+++ bvqy13_web_v1/assets/config.js
@@ -2,10 +2,11 @@ window.BV13_CONFIG={
   hospitalName:"Bệnh viện Quân y 13",
   address:"54 An Dương Vương, phường Quy Nhơn Nam, tỉnh Gia Lai",
   hotline:"",
+  sourceSheet:"https://docs.google.com/spreadsheets/d/1RF9pkT3NUYjCE3yucDVNLYkc8jSTVakzhWIRTvmvo3I/edit",
   // Khi Google Sheets sẵn sàng, dán URL CSV của từng sheet vào đây.
   // Để trống thì website dùng dữ liệu mẫu tích hợp bên dưới.
   sheets:{
-    departments:"https://docs.google.com/spreadsheets/d/e/2PACX-1vQgZcyBUCTqKEhkZeDU9OOSx-fI4ZrGWhQ2C7Jox-o99aTt2eiKnrtpx4Jhx9dX6g/pub?gid=1522592803&single=true&output=csv",
+    departments:"",
     articles:"",services:"",guides:""
   }
 };
